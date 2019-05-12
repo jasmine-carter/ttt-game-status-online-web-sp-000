354d6d59fa920f4714db7e539a96c0bf79dfcc30
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-12 15:46:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -66,7 +66,7 @@ win_combination = won?(board)
   position_1 = board[win_index_1]
   position_2 = board[win_index_2]
   position_3 = board[win_index_3]
-  if draw?(board) || !won?(board)
+  if draw?(board) || !won?(board) || !full?(board)
     puts nil
   else won?(board)
   position_1
